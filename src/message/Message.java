7be2567ca373543c4cc102c1e78fddb344c77c88
ZPaulo <jose.p.s.olive@gmail.com>
2015-06-04 16:50:24 +0100

    starting on building messages

--- src/message/Message.java
+++ src/message/Message.java
@@ -3,21 +3,5 @@ package message;
 import java.io.IOException;
 
 public class Message {
-
-	public static void main(String[] args) throws InterruptedException, IOException {
-		
-
-		Read bRead= new Read(8000);
-		bRead.start();
-		
-		Read rRead= new Read(8001);
-		rRead.start();
-
-		BParser bparser = new BParser(bRead.messages);
-		bparser.start();
-		
-		RParser rparser = new RParser(rRead.messages);
-		rparser.start();
-
-	}
+	
 }
