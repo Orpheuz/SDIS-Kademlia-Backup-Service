2814e7a704dbb737520f6e61da6202dd16716bb9
ZPaulo <jose.p.s.olive@gmail.com>
2015-06-04 02:30:29 +0100

    asd

--- src/node/NodeTriplet.java
+++ src/node/NodeTriplet.java
@@ -1,29 +1,24 @@
 package node;
 
+import java.net.InetAddress;
+
 
 public class NodeTriplet {
-<<<<<<< HEAD
-	int id, port;
-	String ip;
-	NodeTriplet(int id, int port, String ip) {
-		this.id = id;
-=======
 	byte[] id;
 	int port;
 	InetAddress ip;
 	NodeTriplet(byte[] iD2, int port, InetAddress ip) {
 		this.id = iD2;
->>>>>>> branch 'master' of https://github.com/Orpheuz/SDIS-Kademlia-Backup-Service.git
 		this.ip = ip;
 		this.port = port;
 	}
-	public int getId() {
+	public byte[] getId() {
 		return id;
 	}
 	public int getPort() {
 		return port;
 	}
-	public String getIp() {
+	public InetAddress getIp() {
 		return ip;
 	}
 	
--- src/protocols/Kademlia.java
+++ src/protocols/Kademlia.java
@@ -9,8 +9,8 @@ public class Kademlia {
 
 	public static boolean Ping(/*NodeTriplet sender,*/ NodeTriplet receiver,int timeInterval) throws IOException{
 
-		String response = Write.send("ping", receiver.getIp(), receiver.getPort(), timeInterval);
-		System.out.println(response);
+		//String response = Write.send("ping", receiver.getIp(), receiver.getPort(), timeInterval);
+		//System.out.println(response);
 		return true;
 	}
 
