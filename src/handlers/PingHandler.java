bd7a4f395e43f26935245bde8f075ce034e45dfc
Vítor Teixeira <vitor_filipe_@hotmail.com>
2015-06-05 16:04:40 +0100

    dsadasdas

--- src/handlers/PingHandler.java
+++ src/handlers/PingHandler.java
@@ -1,10 +1,17 @@
 package handlers;
 
+import message.PingMessage;
+
 public class PingHandler implements Runnable {
+
+	PingMessage message;
+	
+	public PingHandler(PingMessage message) {
+		this.message = message;
+	}
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
 		
 	}
 
