f5f87b47723e618f5853393a086c7bab4d9a7bca
Joao Oliveira <joaoliveira117@gmail.com>
2015-06-05 16:51:13 +0100

    sync

--- src/subprotocols/Backup.java
+++ src/subprotocols/Backup.java
@@ -1,38 +1,30 @@
 package subprotocols;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.UnknownHostException;
 
-import utils.Files;
-
 public class Backup implements Runnable {
 
-	private Files file;
+	private File file;
 	private final static int CHUNK_SIZE = 64000;
-	public Backup(Files file) throws UnknownHostException {
+	public Backup(File file, int replicationDegree) throws UnknownHostException {
 		this.file = file;
 	}
 
+	//gerar array a partir do file id
 	@Override
 	public void run() {
 		System.out.println("Running backup thread");
 		try {
 			@SuppressWarnings("resource")
-			FileInputStream fs = new FileInputStream(file.getFile());
+			FileInputStream fs = new FileInputStream(file);
 			int counter = 0;
 			byte[] buffer = new byte[CHUNK_SIZE];
 
 			while (fs.read(buffer) > 0) {
-				boolean timeout = true;
-				
 				sendPutchunk(counter, new String(buffer));
-				
-				if(timeout){
-					System.out.println("timeout");
-					return;
-				}
-				counter++;
 			}
 			fs.close();
 			System.out.println("done");
@@ -45,11 +37,15 @@ public class Backup implements Runnable {
 	
 	public void sendPutchunk(int chunkNo, String body) throws IOException{
 		System.out.println("Sent putchunk " + chunkNo);
+		
+		Lookup lp = new Lookup(target);
+		lp.run();
 		/*Main.getAnonMCSocket().setAddress(InetAddress.getByName(Main.BACKUP_ADDRESS), Main.BACKUP_PORT);
 		Main.getAnonMCSocket().send(Message.buildPutChunkMessage(file.getFileID(),
 				String.valueOf(chunkNo),
 				String.valueOf(file.getRepDegree()),
 				body));*/
 		//TODO enviar o PUTCHUNK para o devido peer
+		
 	}
 }
