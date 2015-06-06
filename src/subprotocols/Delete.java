b3a75894e6ea07da23d014dc96f24d12c8042647
Vítor Teixeira <vitor_filipe_@hotmail.com>
2015-06-06 01:05:30 +0100

    sdad

--- src/subprotocols/Delete.java
+++ src/subprotocols/Delete.java
@@ -1,5 +1,26 @@
 package subprotocols;
 
-public class Delete {
-	//TODO discutir amanha
+import java.util.ArrayList;
+
+import tui.TextInterface;
+
+public class Delete implements Runnable {
+
+	String fileID;
+	
+	public Delete(String fileID) {
+		super();
+		this.fileID = fileID;
+	}
+
+	@Override
+	public void run() {
+		ArrayList<byte[]> owners = TextInterface.dht.whoHasIt(fileID);
+		for(int i = 0; i < owners.size(); i++) {
+			Lookup lp = new Lookup(owners.get(i));
+		}
+		//TextInterface.threadManager.submit(new WriteThread());
+	}
+
+	
 }
