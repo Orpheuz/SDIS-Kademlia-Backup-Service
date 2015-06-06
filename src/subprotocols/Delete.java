bf821cbd82dfc0b1b5ab95928d0bbe7de4ec7c81
ZPaulo <jose.p.s.olive@gmail.com>
2015-06-06 01:11:10 +0100

    delete

--- src/subprotocols/Delete.java
+++ src/subprotocols/Delete.java
@@ -1,7 +1,11 @@
 package subprotocols;
 
 import java.util.ArrayList;
+import java.util.TreeSet;
 
+import listeners.WriteThread;
+import message.DeleteMessage;
+import node.Node;
 import tui.TextInterface;
 
 public class Delete implements Runnable {
@@ -18,8 +22,12 @@ public class Delete implements Runnable {
 		ArrayList<byte[]> owners = TextInterface.dht.whoHasIt(fileID);
 		for(int i = 0; i < owners.size(); i++) {
 			Lookup lp = new Lookup(owners.get(i));
+			TreeSet<Node> nodes = lp.run();
+			Node node = nodes.first();
+			DeleteMessage message = new DeleteMessage(fileID);
+			TextInterface.threadManager.submit(new WriteThread(message.getMessage(), node.getIP(), node.getPort()));
+			TextInterface.dht.removeFile(fileID);	
 		}
-		//TextInterface.threadManager.submit(new WriteThread());
 	}
 
 	
